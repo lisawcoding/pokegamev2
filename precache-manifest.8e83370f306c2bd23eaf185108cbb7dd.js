ab4fd510dd3473dda99d527e19b9d46ad0407f2a
lisawcoding <tolisacoding@gmail.com>
2021-04-27 18:19:06 +0800

    Updates

---
+++ precache-manifest.8e83370f306c2bd23eaf185108cbb7dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f269e95fa8e53fd072a9e93f27fa944",
+    "url": "/pokegamev2/index.html"
+  },
+  {
+    "revision": "6beee124db58fcc4e935",
+    "url": "/pokegamev2/static/css/main.4212a952.chunk.css"
+  },
+  {
+    "revision": "2db00707529170146d8d",
+    "url": "/pokegamev2/static/js/2.8a531f4c.chunk.js"
+  },
+  {
+    "revision": "46f91ec18259a53c6f750cd89192fcc8",
+    "url": "/pokegamev2/static/js/2.8a531f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6beee124db58fcc4e935",
+    "url": "/pokegamev2/static/js/main.902da32c.chunk.js"
+  },
+  {
+    "revision": "1bc3a1c3a0d98f42fc25",
+    "url": "/pokegamev2/static/js/runtime-main.5714119a.js"
+  },
+  {
+    "revision": "302b2446cdc48584303600bbb96ab244",
+    "url": "/pokegamev2/static/media/ball.302b2446.svg"
+  },
+  {
+    "revision": "b35d1ac7b69b11f027e5bb9564834613",
+    "url": "/pokegamev2/static/media/fullscreen.b35d1ac7.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pokegamev2/precache-manifest.8e83370f306c2bd23eaf185108cbb7dd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokegamev2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
